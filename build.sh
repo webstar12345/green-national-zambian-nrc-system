bdedadef525329c3eac7cc32afd5738ab7e009e4
Laurent Simoonga <simoongalaurent427@gmail.com>
2025-11-15 00:40:43 +0200

    Add build script for Render deployment

--- build.sh
+++ build.sh
@@ -2,11 +2,7 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate
