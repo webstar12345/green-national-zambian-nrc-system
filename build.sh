360f5063c58073a312bccead0858519b090fc51a
Laurent Simoonga <simoongalaurent427@gmail.com>
2025-11-19 22:30:49 +0200

    Update build script and settings

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Clear any cached templates
+python clear_cache.py || true
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createdefaultadmin
